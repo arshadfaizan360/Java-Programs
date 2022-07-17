ba6816e31f322adc7a9e769dec732bff24cbb38d
Faizan <arshadfaizan360@gmail.com>
2022-07-17 23:42:48 +0100

    Initial commit

---
+++ HelloWorld/src/HelloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+package HelloWorld;
+
+public class HelloWorld {
+    public static void TestMethod() {
+        System.out.println("This is a test");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello world (again)!");
+
+        HelloWorld.TestMethod();
+    }
+}
